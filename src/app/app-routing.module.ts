92878a49b30b919b7ed0f20848137a0092406045
JorderGomes <jordernoveau@gmail.com>
2025-09-07 10:43:00 -0300

    feat: add main page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PuzzlesComponent } from './pages/puzzles/puzzles.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/puzzles', pathMatch: 'full' },
+  { path: 'puzzles', component: PuzzlesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/puzzles/puzzles.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-puzzles',
+  templateUrl: './puzzles.component.html',
+  styleUrl: './puzzles.component.css'
+})
+export class PuzzlesComponent {
+
+}
